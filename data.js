efeae0f0108f5814ba3febe9f4cc85967a3c5271
pawaado <t.takuya36@gmail.com>
2026-07-14 11:05:43 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -4,17 +4,17 @@ window.TEST_DATA = [
     "system": null,
     "type2": "一般細菌",
     "periodic": "－",
-    "routine": "4、1-4ろ過水、6、7、11、15、企水",
-    "dishes": "25",
-    "medium": "375＋25（予備）＝400 mL",
-    "reagent": "9.4 g"
+    "routine": "4（10倍）、4（100倍）、1-4ろ過水、6、7、11、15、企水",
+    "dishes": "28",
+    "medium": "420＋30（予備）＝450 mL",
+    "reagent": "10.58 g"
   },
   {
     "group": "ルーチン",
     "system": null,
     "type2": "嫌気性芽胞菌",
     "periodic": "－",
-    "routine": "4、7、11、15",
+    "routine": "4（100倍）、7、11、15",
     "dishes": "13",
     "medium": "325＋25（予備）＝350 mL",
     "reagent": "25.9 g"
@@ -23,17 +23,17 @@ window.TEST_DATA = [
     "group": "定期試験A",
     "system": "泉系統",
     "type2": "一般細菌",
-    "periodic": "4、7、11、15、31、32、33",
+    "periodic": "4（10倍）、4（100倍）、7、11、15、31、32、33",
     "routine": "オゾン、GAC、6、企水",
-    "dishes": "37",
-    "medium": "555＋45（予備）＝600 mL",
-    "reagent": "14.1 g"
+    "dishes": "40",
+    "medium": "600＋50（予備）＝650 mL",
+    "reagent": "15.28 g"
   },
   {
     "group": "定期試験A",
     "system": "泉系統",
     "type2": "従属栄養細菌",
-    "periodic": "4、7、31",
+    "periodic": "4（100倍）、7、31",
     "routine": "オゾン、GAC、6、11、15",
     "dishes": "28",
     "medium": "420＋30（予備）＝450 mL",
@@ -43,18 +43,18 @@ window.TEST_DATA = [
     "group": "定期試験A",
     "system": "片山系統",
     "type2": "一般細菌",
-    "periodic": "4、7、11、14、15、31、32、33",
+    "periodic": "4（10倍）、4（100倍）、7、11、14、15、31、32、33",
     "routine": "オゾン、GAC、6、企水",
-    "dishes": "40",
-    "medium": "600＋50（予備）＝650 mL",
-    "reagent": "15.28 g"
+    "dishes": "43",
+    "medium": "645＋55（予備）＝700 mL",
+    "reagent": "16.45 g"
   },
   {
     "group": "定期試験A",
     "system": "片山系統",
     "type2": "従属栄養細菌",
     "periodic": "11、14、15、32、33",
-    "routine": "4、オゾン、GAC、6、7",
+    "routine": "4（100倍）、オゾン、GAC、6、7",
     "dishes": "34",
     "medium": "510＋40（予備）＝550 mL",
     "reagent": "10.01 g"
@@ -63,38 +63,38 @@ window.TEST_DATA = [
     "group": "定期試験A",
     "system": "企業団系統",
     "type2": "一般細菌",
-    "periodic": "4、7、11、15、31、32、33",
+    "periodic": "4（10倍）、4（100倍）、7、11、15、31、32、33",
     "routine": "6、企水",
-    "dishes": "31",
-    "medium": "465＋35（予備）＝500 mL",
-    "reagent": "11.75 g"
+    "dishes": "34",
+    "medium": "510＋40（予備）＝550 mL",
+    "reagent": "12.93 g"
   },
   {
     "group": "定期試験B",
     "system": "泉・片山系統",
     "type2": "一般細菌",
     "periodic": "34、35、36、37、38、39、41",
-    "routine": "4、6、7、11、15、企水",
-    "dishes": "43",
-    "medium": "645＋55（予備）＝700 mL",
-    "reagent": "16.45 g"
+    "routine": "4（10倍）、4（100倍）、6、7、11、15、企水",
+    "dishes": "46",
+    "medium": "690＋60（予備）＝750 mL",
+    "reagent": "17.63 g"
   },
   {
     "group": "定期試験B",
     "system": "企業団系統",
     "type2": "一般細菌",
     "periodic": "34、35、36、37、38、39、41",
-    "routine": "4、オゾン、GAC、6、7、11、15、企水",
-    "dishes": "49",
-    "medium": "735＋65（予備）＝800 mL",
-    "reagent": "18.8 g"
+    "routine": "4（10倍）、4（100倍）、オゾン、GAC、6、7、11、15、企水",
+    "dishes": "52",
+    "medium": "780＋70（予備）＝850 mL",
+    "reagent": "19.98 g"
   },
   {
     "group": "定期試験B",
     "system": "企業団系統",
     "type2": "従属栄養細菌",
     "periodic": "34、35、36、37、38、39、41",
-    "routine": "4、オゾン、GAC、6、7、11、15",
+    "routine": "4（100倍）、オゾン、GAC、6、7、11、15",
     "dishes": "46",
     "medium": "690＋60（予備）＝750 mL",
     "reagent": "13.65 g"
@@ -103,10 +103,10 @@ window.TEST_DATA = [
     "group": "定期試験AB（企業団系統）",
     "system": null,
     "type2": "一般細菌",
-    "periodic": "4、7、11、15、31、32、33、34、35、36、37、38、39、41",
+    "periodic": "4（10倍）、4（100倍）、7、11、15、31、32、33、34、35、36、37、38、39、41",
     "routine": "6、企水",
-    "dishes": "52",
-    "medium": "780＋70（予備）＝850 mL",
+    "dishes": "55",
+    "medium": "825＋75（予備）＝900 mL",
     "reagent": "19.98 g"
   },
   {
@@ -114,7 +114,7 @@ window.TEST_DATA = [
     "system": null,
     "type2": "従属栄養細菌",
     "periodic": "34、35、36、37、38、39、41",
-    "routine": "4、オゾン、GAC、6、7、11、15",
+    "routine": "4（100倍）、オゾン、GAC、6、7、11、15",
     "dishes": "46",
     "medium": "690＋60（予備）＝750 mL",
     "reagent": "13.65 g"
